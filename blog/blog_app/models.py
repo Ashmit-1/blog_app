496b4fcf8020a47ed81d4efe76989421ff1388eb
Ashmit Das <dasashmit16@gmail.com>
2025-10-19 19:27:06 +0530

    add blog model to write blogs

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    thumbnail = models.ImageField(null=True, blank=True, upload_to='photos/')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.content[:15]}"
+    
