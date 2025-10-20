8b4b210615eb8612c95e173c0aa06644a27bcbef
Ashmit Das <dasashmit16@gmail.com>
2025-10-20 17:07:12 +0530

    give delete post and edit post functionality

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.blog, name="blog"),
     path('create_post/', views.create_post, name="create_post"),
+    path('<int:form_id>/edit_post/', views.edit_post, name="edit_post"),
+    path('<int:form_id>/delete_post/', views.delete_post, name="delete_post"),
+
 ] 
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BlogForm
 from .models import Blog
 
@@ -10,13 +10,32 @@ def blog(request):
 def create_post(request):
     if request.method == "POST":
         form = BlogForm(request.POST, request.FILES)
-        print("POST request received Block")
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user 
             post.save()
-            print("Redirect Block")
             return redirect('blog')
     else:
         form = BlogForm()
     return render(request, "blog_app/post_form.html", {"form" : form})
+
+def edit_post(request, form_id):
+    post = get_object_or_404(Blog, pk=form_id, user=request.user)
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user 
+            post.save()
+            return redirect('blog')
+    else:
+        form = BlogForm(instance=post)
+    return render(request, "blog_app/post_form.html", {"form" : form})
+
+def delete_post(request, form_id):
+    post = get_object_or_404(Blog, pk=form_id, user=request.user)
+    if request.method=="POST":
+        post.delete()
+        return redirect('blog')
+    return render(request, 'blog_app/confirm_delete.html', {"post" : post})
+
