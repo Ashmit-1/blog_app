4c2039e45b5aeff527db99f5c36fa651709122bc
Ashmit Das <dasashmit16@gmail.com>
2025-10-20 11:32:18 +0530

    modify blog app to show all blogs

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .forms import BlogForm
+from .models import Blog
 
 # Create your views here.
 def blog(request):
-    return render(request, "blog_app/index.html")
+    all_blogs = Blog.objects.all()
+    return render(request, "blog_app/index.html", {"all_blogs" : all_blogs})
