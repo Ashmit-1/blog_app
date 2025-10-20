8b1b7349c2c92b1b89ad8a3f58cf803ed0154209
Ashmit Das <dasashmit16@gmail.com>
2025-10-20 12:15:25 +0530

    create create post views, urls, forms

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name="blog"),
+    path('create_post/', views.create_post, name="create_post"),
 ] 
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import BlogForm
 from .models import Blog
 
 # Create your views here.
 def blog(request):
-    all_blogs = Blog.objects.all()
+    all_blogs = Blog.objects.all().order_by('-thumbnail')
     return render(request, "blog_app/index.html", {"all_blogs" : all_blogs})
+
+def create_post(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES)
+        print("POST request received Block")
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user 
+            post.save()
+            print("Redirect Block")
+            return redirect('blog')
+    else:
+        form = BlogForm()
+    return render(request, "blog_app/post_form.html", {"form" : form})
